998bfe391ee8f9fc79681ae5ae134c7997f0e442
Ravi kumar <121974935+ravikumar9555@users.noreply.github.com>
2023-08-24 21:53:45 +0530

    Time: 25 ms (25.99%), Space: 57 MB (28.58%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,21 +1,43 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-          Map<Integer,Integer> mp=new HashMap<>();
-        for(int x: nums){
-           if(mp.containsKey(x)){
-                mp.put(x,mp.get(x)+1);
-           }else{
-               mp.put(x,1);
-           }
-        }
+        //   Map<Integer,Integer> mp=new HashMap<>();
+        // for(int x: nums){
+        //    if(mp.containsKey(x)){
+        //         mp.put(x,mp.get(x)+1);
+        //    }else{
+        //        mp.put(x,1);
+        //    }
+        // }
 
-        for(int n:mp.keySet()){
-            int val=mp.get(n);
-            if(val>1){
-                return n;
+        // for(int n:mp.keySet()){
+        //     int val=mp.get(n);
+        //     if(val>1){
+        //         return n;
+        //     }
+        // }
+        // return -1;
+        int n = nums.length - 1;
+    int left = 1;
+    int right = n;
+    
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        
+        int count = 0;
+        for (int num : nums) {
+            if (num <= mid) {
+                count++;
             }
         }
-        return -1;
+        
+        if (count > mid) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    return left;
 
     }
 }
