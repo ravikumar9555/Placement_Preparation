6d8403cb08a5a43563cc16c6dc29e06a4febf9c9
Ravi kumar <121974935+ravikumar9555@users.noreply.github.com>
2023-09-05 11:40:31 +0530

    Time: 0 ms (100%), Space: 42.6 MB (98.79%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,16 +1,58 @@
+// class Solution {
+//   public Node copyRandomList(Node head) {
+//     if (head == null)
+//       return null;
+//     if (map.containsKey(head))
+//       return map.get(head);
+
+//     Node newNode = new Node(head.val);
+//     map.put(head, newNode);
+//     newNode.next = copyRandomList(head.next);
+//     newNode.random = copyRandomList(head.random);
+//     return newNode;
+//   }
+
+//   private Map<Node, Node> map = new HashMap<>();
+// }
 class Solution {
-  public Node copyRandomList(Node head) {
-    if (head == null)
-      return null;
-    if (map.containsKey(head))
-      return map.get(head);
 
-    Node newNode = new Node(head.val);
-    map.put(head, newNode);
-    newNode.next = copyRandomList(head.next);
-    newNode.random = copyRandomList(head.random);
-    return newNode;
-  }
-
-  private Map<Node, Node> map = new HashMap<>();
+   public Node copyRandomList(Node head) {
+        if(head== null)
+            return null;
+        
+        Node curr = head;
+        
+        while(curr!=null) {
+            Node currNext = curr.next;
+            curr.next = new Node(curr.val);
+            curr.next.next = currNext;
+            curr = currNext;
+        }
+        
+        //Deep copy of random pointers
+        curr = head;
+        while(curr!=null && curr.next!=null) {
+            if(curr.random == null) {
+                curr.next.random = null;
+            } else {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+        
+        //Deep copy of next pointers and recovering old linked list
+        Node newHead = head.next;
+        Node newCurr = newHead;
+        curr          = head;
+        while(curr!=null && newCurr!=null) {
+            curr.next    = curr.next==null ? null : curr.next.next;
+            
+            newCurr.next = newCurr.next==null ? null : newCurr.next.next;
+            
+            curr = curr.next;
+            newCurr = newCurr.next;
+        }
+       
+        return newHead;        
+    }
 }
