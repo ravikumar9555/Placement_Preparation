43c55642bc742a6927db4b0951f8405fdce2fc49
Ravi kumar <121974935+ravikumar9555@users.noreply.github.com>
2023-09-19 17:34:54 +0530

    Time: 4 ms (87.14%), Space: 56.4 MB (67.3%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,43 +1,20 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-        //   Map<Integer,Integer> mp=new HashMap<>();
-        // for(int x: nums){
-        //    if(mp.containsKey(x)){
-        //         mp.put(x,mp.get(x)+1);
-        //    }else{
-        //        mp.put(x,1);
-        //    }
-        // }
+  public int findDuplicate(int[] nums) {
+    int slow = nums[nums[0]];
+    int fast = nums[nums[nums[0]]];
 
-        // for(int n:mp.keySet()){
-        //     int val=mp.get(n);
-        //     if(val>1){
-        //         return n;
-        //     }
-        // }
-        // return -1;
-        int n = nums.length - 1;
-    int left = 1;
-    int right = n;
-    
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-        
-        int count = 0;
-        for (int num : nums) {
-            if (num <= mid) {
-                count++;
-            }
-        }
-        
-        if (count > mid) {
-            right = mid;
-        } else {
-            left = mid + 1;
-        }
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
     }
-    
-    return left;
 
+    slow = nums[0];
+
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
     }
+
+    return slow;
+  }
 }
